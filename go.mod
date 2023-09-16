ff3a482946abf32faf0db6e363463e27fa9cb54a
ricardo <ricardorbxx1@gmail.com>
2023-09-16 17:58:42 -0300

    Seno cosseno, oq vc quiser, é só pedir gatinha

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ricardoraposo/raygo
 
-go 1.20
+go 1.21
 
 require (
 	github.com/gen2brain/raylib-go/raylib v0.0.0-20230826160040-f770ca098119
--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	width := int32(800)
-	heigth := int32(600)
+	width := int32(1200)
+	heigth := int32(800)
 	rl.InitWindow(width, heigth, "Blueberry Raygo")
 	defer rl.CloseWindow()
 
@@ -20,29 +20,42 @@ func main() {
 	angle := 0.0
 
 	var pathPoints []rl.Vector2
+	var pathReversedPoints []rl.Vector2
 	for !rl.WindowShouldClose() {
-		circleX := int32(1.5 * amplitude * math.Cos(angle))
-		circleY := int32(amplitude * math.Cos(angle*3))
+		// circleX := int32(-2 * amplitude * math.Sin(angle))
+		circleX := int32(amplitude * angle)
+		circleY := int32(-amplitude * math.Sin(angle*7))
+		// circleY := int32(math.Sin(float64(circleX)) * amplitude / 3)
 		angle += speed
 
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.LightGray)
 
-    // Draws the x and y axis
-		rl.DrawLineEx(rl.Vector2{X: 400, Y: 0}, rl.Vector2{X: 400, Y: 600}, 2, rl.Black)
-		rl.DrawLineEx(rl.Vector2{X: 0, Y: 300}, rl.Vector2{X: 800, Y: 300}, 2, rl.Black)
+		// Draws the x and y axis
+		rl.DrawLineEx(rl.Vector2{X: float32(width / 2), Y: 0}, rl.Vector2{X: float32(width / 2), Y: float32(heigth)}, 2, rl.Black)
+		rl.DrawLineEx(rl.Vector2{X: 0, Y: float32(heigth / 2)}, rl.Vector2{X: float32(width), Y: float32(heigth / 2)}, 2, rl.Black)
 
-    // Draws the line that will follow the circle
-		start := rl.Vector2{X: float32(400 + circleX), Y: 300}
-		end := rl.Vector2{X: float32(400 + circleX), Y: float32(300 + circleY)}
-		rl.DrawLineEx(start, end, 3, rl.Red)
+		// Draws the line that will follow the circle
+		rls := rl.Vector2{X: float32(width/2 + circleX), Y: float32(heigth / 2)}
+		rle := rl.Vector2{X: float32(width/2 + circleX), Y: float32(heigth/2 + circleY)}
+		lls := rl.Vector2{X: float32(width/2 - circleX), Y: float32(heigth / 2)}
+		lle := rl.Vector2{X: float32(width/2 - circleX), Y: float32(heigth/2 - circleY)}
+		rl.DrawLineEx(rls, rle, 3, rl.Red)
+		rl.DrawLineEx(lls, lle, 3, rl.Red)
 
-		currentPosition := rl.Vector2{X: float32(400 + circleX), Y: float32(300 + circleY)}
-    pathPoints = append(pathPoints, currentPosition)
-		rl.DrawCircle(400+circleX, 300+circleY, float32(circleRadius), rl.Red)
+    // Current position of the balls
+		cp := rl.Vector2{X: float32(width/2 + circleX), Y: float32(heigth/2 + circleY)}
+		crp := rl.Vector2{X: float32(width/2 - circleX), Y: float32(heigth/2 - circleY)}
 
-    for i := 1; i < len(pathPoints); i++ {
+		pathPoints = append(pathPoints, cp)
+		pathReversedPoints = append(pathReversedPoints, crp)
+
+		rl.DrawCircle(int32(cp.X), int32(cp.Y), float32(circleRadius), rl.Red)
+		rl.DrawCircle(int32(crp.X), int32(crp.Y), float32(circleRadius), rl.Red)
+
+		for i := 1; i < len(pathPoints); i++ {
 			rl.DrawLineEx(pathPoints[i-1], pathPoints[i], 4, rl.Black)
+			rl.DrawLineEx(pathReversedPoints[i-1], pathReversedPoints[i], 4, rl.Black)
 		}
 
 		rl.EndDrawing()
