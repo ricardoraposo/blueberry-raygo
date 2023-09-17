5c6c010380694b20cf7ebdffbe36358bebf4ae1f
ricardo <ricardorbxx1@gmail.com>
2023-09-17 16:55:57 -0300

    drag and drop

--- main.go
+++ main.go
@@ -20,12 +20,11 @@ func main() {
 
 	// Music stuff
 	rl.InitAudioDevice()
-	music := rl.LoadMusicStream("crab_rave.mp3")
-	rl.PlayMusicStream(music)
+	var music rl.Music
 
 	// Wave stuff
-	wave := rl.LoadWave("crab_rave.mp3")
-	allSamples := rl.LoadWaveSamples(wave)
+	var wave rl.Wave
+	var allSamples []float32
 
 	circleRadius := 2 * math.Pi
 	amplitude := 200.0
@@ -34,73 +33,94 @@ func main() {
 	angle := 0.0
 
 	var pathPoints []rl.Vector2
-	var pathReversedPoints []rl.Vector2
+	// var pathReversedPoints []rl.Vector2
+	font := rl.LoadFont("pragmasevka-regular.ttf")
 
 	average := float32(0)
-	step := 750
+	step := 0
 	i := 0
 	// Event loop start
 	for !rl.WindowShouldClose() {
-		rl.UpdateMusicStream(music)
-
-		slice := allSamples[i : i+step]
-		sum := float32(0.0)
-		for k := 0; k < len(slice); k++ {
-			sum = sum + slice[k]
+		if rl.IsFileDropped() {
+			droppedFile := rl.LoadDroppedFiles()
+			music = rl.LoadMusicStream(droppedFile[0])
+			rl.PlayMusicStream(music)
+			wave = rl.LoadWave(droppedFile[0])
+			allSamples = rl.LoadWaveSamples(wave)
+			step = len(allSamples) / 1024
 		}
-
-		average = sum / float32(len(slice))
-
-		i = i + step
 
 		if rl.IsKeyPressed(rl.KeySpace) {
 			if rl.IsMusicStreamPlaying(music) {
 				rl.PauseMusicStream(music)
 				speed = 0
+				step = 0
 			} else {
 				rl.ResumeMusicStream(music)
+				step = len(allSamples) / 1024
 				speed = acc
 			}
 		}
 
-		// circleX := int32(2 * amplitude * math.Sin(angle))
-		circleX := int32(amplitude * angle)
-		circleY := average
-		// circleY := int32(-amplitude * math.Sin(angle*10))
-		angle += speed
+		if rl.IsMusicStreamPlaying(music) {
+			rl.UpdateMusicStream(music)
 
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.LightGray)
+			slice := allSamples[i : i+step]
+
+			sum := float32(0.0)
+			for k := 0; k < len(slice); k++ {
+				sum = sum + slice[k]
+			}
+			average = sum / float32(len(slice))
+			i = i + step
+
+			// Drawing stuff
+			rl.BeginDrawing()
+			rl.ClearBackground(rl.LightGray)
+			circleX := int32(amplitude * angle)
+			circleY := average
+			angle += speed
+			// circleX := int32(2 * amplitude * math.Sin(angle))
+			// circleY := int32(-amplitude * math.Sin(angle*10))
+			// rls := rl.Vector2{X: float32(width / 2), Y: float32(heigth / 2)}
+			// rle := rl.Vector2{X: float32(width / 2), Y: float32(heigth / 2)}
+			// rl.DrawLineEx(rls, rle, 3, rl.Red)
+
+			cp := rl.Vector2{X: float32(width/2 + circleX), Y: float32(heigth/2) * circleY}
+			// crp := rl.Vector2{X: float32(width/2 - circleX), Y: float32(heigth/2) - circleY}
+
+			rl.BeginMode2D(camera)
+			camera.Target = cp
+
+			pathPoints = append(pathPoints, cp)
+			// pathReversedPoints = append(pathReversedPoints, crp)
+
+			rl.DrawCircle(int32(cp.X), int32(cp.Y), float32(circleRadius), rl.Red)
+			// rl.DrawCircle(int32(crp.X), int32(crp.Y), float32(circleRadius), rl.Red)
+
+			for i := 1; i < len(pathPoints); i++ {
+				rl.DrawLineEx(pathPoints[i-1], pathPoints[i], 4, rl.Black)
+				// rl.DrawLineEx(pathReversedPoints[i-1], pathReversedPoints[i], 4, rl.Black)
+			}
+		} else {
+			rl.ClearBackground(rl.LightGray)
+			text := "Drag and drop files to play"
+			rl.DrawTextEx(
+				font,
+				text,
+				rl.Vector2{X: float32(width/2 - int32(len(text)/2)*16), Y: float32(heigth/2) - 16},
+				32.0,
+				1.0,
+				rl.Black)
+		}
 
 		// Draws the x and y axis
-		rl.DrawLineEx(rl.Vector2{X: float32(width / 2), Y: 0}, rl.Vector2{X: float32(width / 2), Y: float32(heigth)}, 2, rl.Black)
-		rl.DrawLineEx(rl.Vector2{X: 0, Y: float32(heigth / 2)}, rl.Vector2{X: float32(width), Y: float32(heigth / 2)}, 2, rl.Black)
-
+		// rl.DrawLineEx(rl.Vector2{X: float32(width / 2), Y: 0}, rl.Vector2{X: float32(width / 2), Y: float32(heigth)}, 2, rl.Black)
+		// rl.DrawLineEx(rl.Vector2{X: 0, Y: float32(heigth / 2)}, rl.Vector2{X: float32(width), Y: float32(heigth / 2)}, 2, rl.Black)
 		// Draws the line that will follow the circle
-		rls := rl.Vector2{X: float32(width / 2), Y: float32(heigth / 2)}
-		rle := rl.Vector2{X: float32(width / 2), Y: float32(heigth / 2)}
 		// lls := rl.Vector2{X: float32(width/2 - circleX), Y: float32(heigth / 2)}
 		// lle := rl.Vector2{X: float32(width/2 - circleX), Y: float32(heigth/2 - circleY)}
-		rl.DrawLineEx(rls, rle, 3, rl.Red)
 		// rl.DrawLineEx(lls, lle, 3, rl.Red)
-
-		// Current position of the balls
-		cp := rl.Vector2{X: float32(width/2 + circleX), Y: float32(heigth/2) * circleY}
-		crp := rl.Vector2{X: float32(width/2 - circleX), Y: float32(heigth/2) - circleY}
-
-		rl.BeginMode2D(camera)
-		camera.Target = cp
-
-		pathPoints = append(pathPoints, cp)
-		pathReversedPoints = append(pathReversedPoints, crp)
-
-		rl.DrawCircle(int32(cp.X), int32(cp.Y), float32(circleRadius), rl.Red)
-		rl.DrawCircle(int32(crp.X), int32(crp.Y), float32(circleRadius), rl.Red)
-
-		for i := 1; i < len(pathPoints); i++ {
-			rl.DrawLineEx(pathPoints[i-1], pathPoints[i], 4, rl.Black)
-			rl.DrawLineEx(pathReversedPoints[i-1], pathReversedPoints[i], 4, rl.Black)
-		}
 
 		rl.EndDrawing()
 	}
